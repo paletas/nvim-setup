3163e9842e266097c0d42bc67e04b728acd081a6
João Silvestre <joao.silvestre@outlook.com>
2023-09-04 22:20:58 +0100

    changing yanking to always copy to the system clipboard

--- lua/paletas/init.lua
+++ lua/paletas/init.lua
@@ -2,6 +2,8 @@ require("paletas.remap")
 require("paletas.theme")
 require("paletas.packer")
 
+vim.opt.clipboard = 'unnamedplus'
+
 print("Temporary Reminder, remove this soon-ish!")
 print("<leader> is <space>, <C-> means Control")
 print("<leader>pv for the file explorer")
